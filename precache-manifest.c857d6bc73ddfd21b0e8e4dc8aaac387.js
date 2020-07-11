45a9775290981f1925341d64429a611f499aa8e3
noe2017 <noeandimat611@gmail.com>
2020-07-12 04:23:29 +0800

    Updates

---
+++ precache-manifest.c857d6bc73ddfd21b0e8e4dc8aaac387.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95eebd48f17132b6e4e269f7f11aa13a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "8e3561eec9e0b657ca45",
+    "url": "/contactmanager/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "35388a2421f129281ae3",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "8e3561eec9e0b657ca45",
+    "url": "/contactmanager/static/js/2.714ee64b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.714ee64b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35388a2421f129281ae3",
+    "url": "/contactmanager/static/js/main.4ba81d44.chunk.js"
+  },
+  {
+    "revision": "78370bb576dc7a6cd44e",
+    "url": "/contactmanager/static/js/runtime-main.cdf6a9fb.js"
+  }
+]);

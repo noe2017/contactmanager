7089094ee55503e66df9c8b8e26b629dca7aa0d6
noe2017 <noeandimat611@gmail.com>
2020-07-12 04:02:29 +0800

    Initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <div>
+      <div>
+        <h1 className="display-4">About Contact Manager</h1>
+        <p className="lead">Simple App to Manage Contacts</p>
+        <p className="text-secondary">Version 1.0.0</p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
